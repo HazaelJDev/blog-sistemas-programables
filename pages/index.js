b3730ec0251b2c47c9a10b242adc641ab2e01961
Hazael Jiménez <viajero25057@innovaccion.mx>
2021-01-04 14:22:07 -0600

    add: Agregando información de los temas

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,31 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>In love with Computing and programming since 2011. Computer Systems Engineering Student.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>El microcontrolador ejecuta el programa cargado en la memoria Flash. Esto se
+          denomina el código ejecutable y está compuesto por una serie de ceros y unos,
+          aparentemente sin significado. Dependiendo de la arquitectura del microcontrolador, el código binario está compuesto por palabras de 12, 14 o 16
+          bits de anchura. Cada palabra se interpreta por la CPU como una instrucción a ser
+          ejecutada durante el funcionamiento del microcontrolador. Todas las instrucciones
+          que el microcontrolador puede reconocer y ejecutar se les denominan
+          colectivamente Conjunto de instrucciones. Como es más fácil trabajar con el
+          sistema de numeración hexadecimal, el código ejecutable se representa con
+          frecuencia como una serie de los números hexadecimales denominada código
+          Hex. En los microcontroladores PIC con las palabras de programa de 14 bits de
+          anchura, el conjunto de instrucciones tiene 35 instrucciones diferentes.
+      </p>
+      <p>{`La operación de escritura, utilizando los puertos puede ser realizada por cualquiera de ellos, no
+obstante, el puerto PO es el que presenta una mayor cargabilidad, permitiendo comandar ocho
+cargas TTL - LS, mientras que los otros tres permiten cuatro cargas TTL – LS.
+Para comandar cargas de mayor consumo energético, como relés, se recomienda utilizar, entre el
+puerto y la carga drivers no inversores. Para la operación de
+ESCRITURA en el puerto, la instrucción más habitual es la siguiente:
+MOV PX , <dato> ; PX<- dato.
+X toma valores 0, 1, 2 y 3 según el puerto.
+Admitiendo <dato> todos los tipos de direccionamiento.
+Si se necesita activar o desactivar un bit de un puerto ( se puede hacer extensivo a todos los
+registros direccionales bit a bit del SFR), puede hacerlo utilizando las instrucciones booleanas.`}</p>
       </section>
+      ..3333
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
